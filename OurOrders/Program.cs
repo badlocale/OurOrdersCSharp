cbeac875af10459fa0a29bf93f6b3a6b96a75953
Kopchigashev-Ilya <ilya.kopchigashev@gmail.com>
2024-06-13 17:41:38 +0300

    Task done

---
+++ OurOrders/Program.cs
@@ -0,0 +1,51 @@
+﻿#region usings
+
+using OurOrdersRunner;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+#endregion
+
+namespace OurOrders
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            SolutionChecker.Check(new Processor());
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        ///     Реализация алгоритма задачи "Свои заявки"
+        /// </summary>
+        internal sealed class Processor : IOrderLogProcessor
+        {
+            private readonly IList<OrderLogEntry> _entries = new List<OrderLogEntry>();
+
+            public void Update(OrderLogEntry orderLogEntry)
+            {
+                var oldEntry = _entries.FirstOrDefault(o => o.Id == orderLogEntry.Id);
+                if (oldEntry != null)
+                {
+                    _entries.Remove(oldEntry);
+                }
+
+                if (orderLogEntry.Deleted || orderLogEntry.Volume is 0)
+                {
+                    return;
+                }
+
+                _entries.Add(orderLogEntry);
+            }            
+
+            public IEnumerable<Tuple<decimal, long>> GetLevels()
+            {
+                return _entries
+                    .GroupBy(o => o.Price, o => o.Volume)
+                    .Select(o => new Tuple<decimal, long>(o.Key, o.Sum()));
+            }
+        }
+    }
+}
